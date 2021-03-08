1b605e04369fd65a2c0ab08e604bbf8de9747cf2
TigerZh <only_tigerhu@163.com>
2021-03-08 19:18:09 +0800

    await

--- src/pages/Encoding/Encoding.tsx
+++ src/pages/Encoding/Encoding.tsx
@@ -11,7 +11,16 @@ const useStyles = makeStyles((theme: Theme) =>
 export const Encoding: React.FC = () => {
     const classes = useStyles()
     return <DefaultLayout >
-        <div className={classes.root}></div>
+        <div className={classes.root}>
+            <div className="p-6 max-w-sm mx-auto bg-white rounded-xl flex items-center space-x-4">
+                <div className="flex-shrink-0">
+                </div>
+                <div>
+                    <div className="text-xl font-medium text-black">ChitChat</div>
+                    <p className="text-gray-500">You have a new message!</p>
+                </div>
+            </div>
+        </div>
     </DefaultLayout >
 };
 
